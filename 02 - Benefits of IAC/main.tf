9db6eaab62b9ff0f965cd6617cb6c80c7351e4d4
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2024-11-05 06:22:23 -0600

    varriables for VPC

--- 02 - Benefits of IAC/main.tf
+++ 02 - Benefits of IAC/main.tf
@@ -187,9 +187,9 @@ resource "random_id" "randomness" {
 
 resource "aws_subnet" "variables-subnet" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.250.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
+  cidr_block              = var.variables_sub_cidr
+  availability_zone       = var.variables_sub_az
+  map_public_ip_on_launch = var.variables_sub_auto_ip
 
   tags = {
     Name      = "sub-variables-us-east-1a"
