2410b87b739027c0b897cfe09e66cd13d5751b8c
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2024-11-04 20:15:24 -0600

    s3 bucket

--- 02 - Benefits of IAC/main.tf
+++ 02 - Benefits of IAC/main.tf
@@ -153,3 +153,20 @@ resource "aws_instance" "web" {
     "Identity" = "tf_testing_server"
   }
 }
+
+# Terraform Resource Block - Build an S
+resource "aws_s3_bucket" "my-new-S3-bucket" {   
+  bucket = "my-new-tf-test-bucket-bryan-s223"
+
+  tags = {     
+    Name = "My S3 Bucket"     
+    Purpose = "Intro to Resource Blocks Lab"   
+  } 
+}
+
+resource "aws_s3_bucket_ownership_controls" "my_new_bucket_acl" {   
+  bucket = aws_s3_bucket.my-new-S3-bucket.id  
+  rule {     
+    object_ownership = "BucketOwnerPreferred"   
+  }
+}
