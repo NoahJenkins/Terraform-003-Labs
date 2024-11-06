48b6717635b25f950c170b8ed52ea974cf01cc78
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2024-11-06 17:09:59 -0600

    TLS provider, set up rsa key, update gitignore to not generate the key.

--- 02 - Main TF Directory/main.tf
+++ 02 - Main TF Directory/main.tf
@@ -217,3 +217,14 @@ resource "aws_subnet" "variables-subnet" {
     Terraform = "true"
   }
 }
+
+#SSH Key
+
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+}
+
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.generated.private_key_pem
+  filename = "MyAWSKey.pem"
+}
--- 02 - Main TF Directory/terraform.tf
+++ 02 - Main TF Directory/terraform.tf
@@ -17,6 +17,10 @@ terraform {
       source  = "hashicorp/local"
       version = ">=2.1.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">=3.0.0"
+    }
   }
 }
 
