29cceeb3f19e4d2022d97afcf91f6c23e64f8aa0
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2024-11-12 16:50:18 -0600

    terraform module sources

--- 05 - Terraform Modules/main.tf
+++ 05 - Terraform Modules/main.tf
@@ -298,22 +298,21 @@ module "server" {
   source    = "./modules/server"
   ami       = data.aws_ami.ubuntu.id
   subnet_id = aws_subnet.public_subnets["public_subnet_3"].id
-  security_groups = [
-    aws_security_group.vpc-ping.id,
+  security_groups = [aws_security_group.vpc-ping.id,
     aws_security_group.ingress-ssh.id,
-    aws_security_group.vpc-web.id
-  ]
+  aws_security_group.vpc-web.id]
 }
 
 module "server_subnet_1" {
-  source    = "./modules/server"
-  ami       = data.aws_ami.ubuntu.id
-  subnet_id = aws_subnet.public_subnets["public_subnet_1"].id
-  security_groups = [
-    aws_security_group.vpc-ping.id,
+  source      = "./modules/web_server"
+  ami         = data.aws_ami.ubuntu.id
+  key_name    = aws_key_pair.generated.key_name
+  user        = "ubuntu"
+  private_key = tls_private_key.generated.private_key_pem
+  subnet_id   = aws_subnet.public_subnets["public_subnet_1"].id
+  security_groups = [aws_security_group.vpc-ping.id,
     aws_security_group.ingress-ssh.id,
-    aws_security_group.vpc-web.id
-  ]
+  aws_security_group.vpc-web.id]
 }
 
 output "public_ip" {
@@ -322,4 +321,29 @@ output "public_ip" {
 
 output "public_dns" {
   value = module.server.public_dns
+}
+
+module "autoscaling" {
+  source = "github.com/terraform-aws-modules/terraform-aws-autoscaling?ref=v8.0.0"
+
+  # Autoscaling group
+  name = "myasg"
+
+  vpc_zone_identifier = [aws_subnet.private_subnets["private_subnet_1"].id,
+    aws_subnet.private_subnets["private_subnet_2"].id,
+  aws_subnet.private_subnets["private_subnet_3"].id]
+  min_size         = 0
+  max_size         = 1
+  desired_capacity = 1
+
+  # Launch template
+
+
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Web EC2 Server 2"
+  }
+
 }
