4a210246d4f5f9a695985a7a5c0a5b525448cbd0
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2024-11-16 14:04:51 -0600

    remote backend

--- 05 - Terraform Modules/terraform.tf
+++ 05 - Terraform Modules/terraform.tf
@@ -1,6 +1,11 @@
 terraform {
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "CCP_Prep"
+
+    workspaces {
+      name = "my_aws_app"
+    }
   }
   required_version = ">= 1.0.0"
   required_providers {
