2cc731fcec62afe6e102101c4d4b150c72424f81
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2024-11-19 19:49:21 -0600

    tf final lab

--- 07 - Read, Gen, and Mod Configs/main.tf
+++ 07 - Read, Gen, and Mod Configs/main.tf
@@ -331,3 +331,20 @@ locals {
     CreatedBy = local.createdby
   }
 }
+
+resource "aws_security_group" "main" {
+  name = "core-sg"
+
+  vpc_id = aws_vpc.vpc.id
+
+  dynamic "ingress" {
+    for_each = var.web_ingress
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+}
--- 07 - Read, Gen, and Mod Configs/variable.tf
+++ 07 - Read, Gen, and Mod Configs/variable.tf
@@ -28,3 +28,28 @@ variable "public_subnets" {
     "public_subnet_3" = 3
   }
 }
+
+variable "web_ingress" {
+  type = map(object(
+    {
+      description = string
+      port        = number
+      protocol    = string
+      cidr_blocks = list(string)
+    }
+  ))
+  default = {
+    "80" = {
+      description = "Port 80"
+      port        = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    "443" = {
+      description = "Port 443"
+      port        = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
